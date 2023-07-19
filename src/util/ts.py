b259ca1b6e92c747ead30def78cce2d162a6c5da
Laszlo Csontos <github@laszlocsontos.com>
2023-07-19 16:14:23 +0200

    Add function to compute coefficients for fraction differentiation

--- src/util/ts.py
+++ src/util/ts.py
@@ -10,3 +10,11 @@ def spectral_entropy(y) -> float:
     ps = np.abs(yf[0:n//2])**2
     ps_norm = ps / ps.sum()
     return stats.entropy(ps_norm)
+
+
+def fracdiff_coef(d: float, n: int) -> np.ndarray:
+    """Computes the coefficients of a fractional differencing filter."""
+    w = np.ones(n)
+    for k in range(1, n):
+        w[k] = -w[k-1] * (d - k + 1) / k
+    return w
